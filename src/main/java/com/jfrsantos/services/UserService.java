94acb9b08fa47ea3f1e6d0188e323c83db45839c
jonathasfrsantos <jonathasfrsantos@gmail.com>
2022-03-27 10:42:22 -0300

    POST method added

--- src/main/java/com/jfrsantos/services/UserService.java
+++ src/main/java/com/jfrsantos/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jfrsantos.domain.User;
+import com.jfrsantos.dto.UserDTO;
 import com.jfrsantos.repositories.UserRepository;
 import com.jfrsantos.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 
