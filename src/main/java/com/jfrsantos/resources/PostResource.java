7c4ca5c719a473476af5ebe54204442767e80cbd
jonathasfrsantos <jonathasfrsantos@gmail.com>
2022-03-27 19:07:13 -0300

    Simple querry method

--- src/main/java/com/jfrsantos/resources/PostResource.java
+++ src/main/java/com/jfrsantos/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.jfrsantos.resources;
 
 import java.net.URI;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jfrsantos.domain.Post;
 import com.jfrsantos.domain.User;
 import com.jfrsantos.dto.UserDTO;
+import com.jfrsantos.resources.util.URL;
 import com.jfrsantos.services.PostService;
 import com.jfrsantos.services.UserService;
 
@@ -30,6 +33,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue ="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 
--- src/main/java/com/jfrsantos/services/PostService.java
+++ src/main/java/com/jfrsantos/services/PostService.java
@@ -1,14 +1,13 @@
 package com.jfrsantos.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jfrsantos.domain.Post;
-import com.jfrsantos.domain.User;
 import com.jfrsantos.repositories.PostRepository;
-import com.jfrsantos.repositories.UserRepository;
 import com.jfrsantos.services.exception.ObjectNotFoundException;
 
 @Service
@@ -20,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 
 
