cd2c5a9c436586ad5c8f76e0883abb1f953ebd7b
jonathasfrsantos <jonathasfrsantos@gmail.com>
2022-03-27 19:34:08 -0300

    Full search

--- src/main/java/com/jfrsantos/resources/PostResource.java
+++ src/main/java/com/jfrsantos/resources/PostResource.java
@@ -1,26 +1,19 @@
 package com.jfrsantos.resources;
 
-import java.net.URI;
-import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jfrsantos.domain.Post;
-import com.jfrsantos.domain.User;
-import com.jfrsantos.dto.UserDTO;
 import com.jfrsantos.resources.util.URL;
 import com.jfrsantos.services.PostService;
-import com.jfrsantos.services.UserService;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -43,6 +36,17 @@ public class PostResource {
 	}
 	
 
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 
 	
 	
